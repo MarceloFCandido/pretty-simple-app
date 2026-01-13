ed4657e97bf52189e3d48b3bca44c3d048d4ccc3
Marcelo F. Cândido <fcandido.marcelo@gmail.com>
2026-01-13 16:09:26 -0300

    Test usage of env groups on preview envs

--- worker/worker.sh
+++ worker/worker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+filepath="/etc/secrets/file-preview-test/test.txt"
+
 echo "Worker starting..."
 
 while true; do
     echo "Worker running at $(date)"
+
+    file_content=$(cat $filepath 2>/dev/null)
+
+    echo "File content: $file_content"
+
     sleep 5
 done
